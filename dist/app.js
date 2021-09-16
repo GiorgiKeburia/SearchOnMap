57f05b32b81b7e2b1593da92f82189826b044cab
giorgikeburia <giorgikeburia1@gmail.com>
2021-09-16 23:53:34 +0400

    final

--- dist/app.js
+++ dist/app.js
@@ -1,40 +1,27 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 const form = document.querySelector('form');
 const addressInput = document.getElementById('address');
 const map = document.getElementById('mapid');
-let myMap = L.map(map).setView([4.0, -72.0], 13);
-function fetchData(name) {
-    return __awaiter(this, void 0, void 0, function* () {
-        let result = yield fetch(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
-            .then((res) => res.json())
-            .then((data) => {
-            const fetchData = [...data];
-            return fetchData[0].latlng;
-        })
-            .catch((error) => console.log('ERROR'));
-        return result;
-    });
-}
-console.log(fetchData('georgia'));
-// console.log(fetchData('georgia'));
+let myMap = L.map(map).setView([10, 10], 2.5);
+L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=Zf18YQYam4x6LDOYNOvP', {
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+}).addTo(myMap);
 function searrchAddressHandler(event) {
     event.preventDefault();
-    console.log(addressInput.value);
-    // const zoomSize: number = 13;
-    // const enteredAddress = addressInput.value;
-    // myMap.setView([100, 200], zoomSize);
-    // renderMap(100, 100, 13);
-    // myMap = L.map(map).setView([45, 45], 3);
-    // let marker = L.marker([51.5, 1]).addTo(myMap);
-    // Send Input Address to Google API!
+    const inputName = addressInput.value;
+    const zoomSize = 7;
+    let result;
+    fetch(`https://restcountries.eu/rest/v2/name/${inputName}?fullText=true`)
+        .then((res) => res.json())
+        .then((data) => {
+        const fetchData = [...data];
+        result = fetchData[0].latlng;
+        let marker = L.marker(result).addTo(myMap);
+        setTimeout(() => {
+            myMap.setView(result, zoomSize);
+        }, 1000);
+    })
+        .catch((error) => console.log('ERROR'));
+    addressInput.value = '';
 }
 form.addEventListener('submit', searrchAddressHandler);
--- src/app.ts
+++ src/app.ts
@@ -2,30 +2,29 @@ const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 const map = document.getElementById('mapid')! as HTMLDivElement;
 
-let myMap = L.map(map).setView([4.0, -72.0], 13);
+let myMap = L.map(map).setView([10, 10], 2.5);
+L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=Zf18YQYam4x6LDOYNOvP', {
+	attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+}).addTo(myMap);
 
-async function fetchData(name: string) {
-	let result = await fetch(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+function searrchAddressHandler(event: Event) {
+	event.preventDefault();
+	const inputName = addressInput.value;
+	const zoomSize: number = 7;
+	let result: any;
+
+	fetch(`https://restcountries.eu/rest/v2/name/${inputName}?fullText=true`)
 		.then((res) => res.json())
 		.then((data) => {
 			const fetchData = [...data];
-			return fetchData[0].latlng;
+			result = fetchData[0].latlng;
+			let marker = L.marker(result).addTo(myMap);
+			setTimeout(() => {
+				myMap.setView(result, zoomSize);
+			}, 1000);
 		})
 		.catch((error) => console.log('ERROR'));
-	return result;
-}
-console.log(fetchData('georgia'));
-// console.log(fetchData('georgia'));
-function searrchAddressHandler(event: Event) {
-	event.preventDefault();
-	console.log(addressInput.value);
-	// const zoomSize: number = 13;
-	// const enteredAddress = addressInput.value;
-	// myMap.setView([100, 200], zoomSize);
-	// renderMap(100, 100, 13);
-	// myMap = L.map(map).setView([45, 45], 3);
-	// let marker = L.marker([51.5, 1]).addTo(myMap);
-	// Send Input Address to Google API!
+	addressInput.value = '';
 }
 
 form.addEventListener('submit', searrchAddressHandler);
